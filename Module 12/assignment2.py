3878fd0ea073096373cdb638924e9ae70b4ef3af
Valdrem <valtsu2003.dre@gmail.com>
2026-03-31 21:43:10 +0300

    assignment2 update

--- Module 12/assignment2.py
+++ Module 12/assignment2.py
@@ -4,14 +4,14 @@ import requests
 city = input("Enter municipality: ")
 
 api_key = "fd2cac36be4631ae03537b90e9494414"
-request_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+request_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
 try:
     response = requests.get(request_url)
     converted_response = response.json()
 
     if response.status_code == 200:
-        print(f"Weather: {converted_response["weather"]["description"]}")
+        print(f"Weather: {converted_response["weather"][0]["description"]}")
         print(f"Weather: {converted_response["main"]["temp"]}")
     else:
         print(f"Error: {response.status_code}")
